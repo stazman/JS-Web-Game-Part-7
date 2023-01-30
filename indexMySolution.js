a37d13ce91dd872f32ef9fd503191aeacb81f0f5
stazman <cadistasio@gmail.com>
2023-01-30 14:16:47 -0600

    Solves activity

---
+++ indexMySolution.js
@@ -0,0 +1,26 @@
+// Create the playable character
+const pc = newPlayableCharacter(100, 110)
+
+// Create a non-playable character
+const npc = newNonPlayableCharacter(50, 300)
+
+// have the NPC start walking east immediately
+// npc.walkEast(1000)
+
+moveNPC()
+
+//You can have the moveNPC function declared here, but you can't call it in the newNonPlayableCharacterMySolution file because the npc instances are created here in the index.js file.
+
+// Create the inventory
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+// Create everything else
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
---
+++ newNonPlayableCharacterMySolution.js
@@ -0,0 +1,83 @@
+function newNonPlayableCharacter(x, y) {
+    let element = newImage('assets/red-character/static.gif')
+    element.style.zIndex = 1;
+
+    let direction = null;
+
+    function moveCharacter() {
+        if (direction === 'west') {
+            x -= 1
+        }
+        if (direction === 'north') {
+            y += 1
+        }
+        if (direction === 'east') {
+            x += 1
+        }
+        if (direction === 'south') {
+            y -= 1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+
+    setInterval(moveCharacter, 1)
+
+    async function walkEast(time) {
+        direction = 'east'
+        element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
+    }
+
+    async function walkNorth(time) {
+        direction = 'north'
+        element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
+    }
+
+    async function walkWest(time) {
+        direction = 'west'
+        element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
+    }
+
+    async function walkSouth(time) {
+        direction = 'south'
+        element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
+    }
+
+    function stop() {
+        direction = null
+        element.src = `./assets/red-character/static.gif`
+    }
+
+    return {
+        element: element,
+        walkWest: walkWest,
+        walkNorth: walkNorth,
+        walkEast: walkEast,
+        walkSouth: walkSouth,
+        stop: stop
+    }
+}
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })
+}
+
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
